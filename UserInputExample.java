67fedd0249b1bc95e2473b3790f499b75cf5cc0c
saroj <saroj.yadav@islingtoncollege.edu.np>
2025-11-20 08:56:30 +0545

    Initial sharing of project

---
+++ UserInputExample.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class UserInputExample here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class UserInputExample
+{
+    public static void main(String[]arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first NUmber");
+        int firstNumber=sc.nextInt();
+        System.out.println("Enter the second Number");
+        int secondNumber=sc.nextInt();
+        
+        int sum=firstNumber+secondNumber;
+        System.out.println("The sum of two value is"+sum);
+        
+            int bigger=(firstNumber>secondNumber)?firstNumber:secondNumber;
+            System.out.println("bigger number is"+bigger);
+            sc.close();
+        
+        
+        
+    }
+}
---
+++ VaraibleExample.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class VaraibleExample here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class VaraibleExample
+{
+    public static void main(String[]arg) // main method
+    {
+        int mobilePrice; // varaiable declare
+        mobilePrice=20000;
+        
+        float bookPrice=500.25f; // intilization
+        
+        String name="Sanskar";
+        String email="eam123@gmail.com";
+        
+        char gender='M';
+        
+        boolean result=true;
+        
+        
+        int firstNumber,secondNumber;
+        firstNumber=30;
+        secondNumber=50;
+        
+        int sum=firstNumber+secondNumber;
+        
+        
+        System.out.println("Sum");
+        System.out.println(sum);
+        System.out.println("The sum of two number is "+sum);
+        
+        
+        
+        
+        
+    }
+    
+}
